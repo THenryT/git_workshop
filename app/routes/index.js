cecf0b1fab210f2e7e2459de673591046a933523
Allen wang <aensheng@gmail.com>
2019-04-17 19:53:22 +1000

    wow

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:id', req, res, next => {
+  res.render('WoW', { title: 'Express' });
 });
 
 module.exports = router;
